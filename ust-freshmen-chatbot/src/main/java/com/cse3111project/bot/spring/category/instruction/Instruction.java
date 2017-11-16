d4e209ae49e4ecae851a1d0dc799e9e56616714f
Chris Liu <chrisliupascal@gmail.com>
2017-11-16 16:17:46 +0800

    Instruction for user done.

---
+++ ust-freshmen-chatbot/src/main/java/com/cse3111project/bot/spring/category/instruction/Instruction.java
@@ -0,0 +1,12 @@
+package com.cse3111project.bot.spring.category.instruction;
+
+import com.cse3111project.bot.spring.category.Category;
+
+public class Instruction
+{
+	private String reply;
+	
+	private static final String DIRECTORY_QUERY = "Tips on HKUST Staff Directory Enquiry\n"
+												+ "You can search details of any valid staff available in UST\n"
+												+ "e.g. Can I know where is the office of Professor Li?"
+}
