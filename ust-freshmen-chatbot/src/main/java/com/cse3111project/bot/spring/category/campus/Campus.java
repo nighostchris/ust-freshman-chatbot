d63f84b76ab3e41ffa2e9949c9ce47fcb920c808
Chris <chrisliupascal@gmail.com>
2017-11-18 13:21:38 +0800

    Javadoc for Campus.java done

--- ust-freshmen-chatbot/src/main/java/com/cse3111project/bot/spring/category/campus/Campus.java
+++ ust-freshmen-chatbot/src/main/java/com/cse3111project/bot/spring/category/campus/Campus.java
@@ -12,18 +12,29 @@ import java.util.ArrayList;
 
 import com.cse3111project.bot.spring.exception.RoomNotFoundException;
 
-// Campus
-//          -> CampusETA
 /**
- * 
+ * The Campus abstract class acts as the main controller for coordinating all the
+ * user-query on estimated time for one to go from one place to another place within
+ * UST campus.
  * @version 1.0
- *
  */
 public abstract class Campus extends Category 
 {
     // public static final String QUERY_KEYWORD[] = CampusETA.CAMPUS_DIRECTION_KEYWORD;
     public static final String QUERY_KEYWORD[] = { "from", "to" };
 
+    /**
+     * This method will take in useful keywords from user-query and analyze about
+     * what are the 2 locations that user is querying for estimated time to walk between.
+     * @param extractedResults This is the only parameter of the function, which is a 
+     * 						   list of processed keyword from the user-query.
+     * @return Category This returns a CampusETA object which contains algorithm for calculating the
+     * 				    estimated time to walk from 1 place to another place in campus.
+     * @throws RoomNotFoundException Throws exception when the room indicated by the user is invalid.
+     * @throws MalformedURLException 
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
     public static Category analyze(final ArrayList<String> extractedResults) 
             throws RoomNotFoundException, MalformedURLException, FileNotFoundException, IOException
     {
