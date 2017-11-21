5f77b974e95a18ad01cc197ab87e2e749425a744
Chris Liu <chrisliupascal@gmail.com>
2017-11-21 17:47:36 +0800

    Javadoc for InstitutionCreditTransferInfo.java done

--- ust-freshmen-chatbot/src/main/java/com/cse3111project/bot/spring/category/academic/credit_transfer/InstitutionCreditTransferInfo.java
+++ ust-freshmen-chatbot/src/main/java/com/cse3111project/bot/spring/category/academic/credit_transfer/InstitutionCreditTransferInfo.java
@@ -1,9 +1,23 @@
 package com.cse3111project.bot.spring.category.academic.credit_transfer;
 
-class InstitutionCreditTransferInfo extends CreditTransferInfo {
+/**
+ * The InstitutionCreditTransferInfo class inherits the CreditTransferInfo class and wrap the details
+ * of the institution course that could be used as credit transfer for user. 
+ * @version 1.0
+ */
+class InstitutionCreditTransferInfo extends CreditTransferInfo 
+{
     private String country;
     private String institution;
 
+    /**
+     * Constructor of the InstitutionCreditTransferInfo.
+     * @param country Country of the institution
+     * @param institution Name of the institution
+     * @param subject Subject name of the course used to transfer for credits
+     * @param transferCourseCode Course code of the transferred course.
+     * @param restriction Restriction for the transfer to apply.
+     */
     InstitutionCreditTransferInfo(String country, String institution, 
                                   String subject, String transferCourseCode, String restriction) {
         super(subject, transferCourseCode, restriction);
